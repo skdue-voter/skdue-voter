52d6e923a08b6e1fbb8d9c52c8945f4156173f60
patkamon <patkamon.a@ku.th>
2022-10-08 14:44:20 +0700

    init

---
+++ components/Navbar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navbar = () => {
+  return <nav className="bg-green-300">Navbar</nav>;
+};
+
+export default Navbar;
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className="border  h-16 bg-purple-300 m-auto mt-10 flex flex-row justify-center">
+      <h1 className="text-red-700 mt-4 ">Hello Skdue Voter</h1>
+    </div>
+  );
+}
