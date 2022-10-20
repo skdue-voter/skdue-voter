0daedb71dba9444a4cb2829603ec6e29facb123c
patkamon <patkamon.a@ku.th>
2022-10-20 12:58:04 +0700

    fix responsive

--- pages/candidate/index.js
+++ pages/candidate/index.js
@@ -44,8 +44,8 @@ function Candidate() {
             // TODO change key to other unique stuff
             <div
               key={index}
-              className={`card flex flex-row border rounded bg-white 
-                // ${index == 4 ? "font-extrabold" : ""}
+              className={`card flex flex-row border rounded bg-white  py-2 max-h-12
+                 ${index == 4 ? "font-semibold" : ""}
                ${
                  index == 1 || index == 7
                    ? " opacity-80 w-2/5 lg:py-3 md:py-1"
@@ -54,36 +54,34 @@ function Candidate() {
               ${
                 index == 0 || index == 8
                   ? " w-1/3 lg:py-2 md:py-0 opacity-60"
-                  : "w-1/2 lg:py-6 md:py-2"
+                  : "w-1/2 lg:py-4 md:py-2"
               } `}
               onClick={(e) => {
                 setMainIndex(card);
                 changeCards(card);
               }}
             >
-              <span className="name ml-3">{card + "_____"}</span>
-              <p className={`${index == 0 || index == 8 ? " text-sm" : ""}`}>
+              <span className="name ml-3">{card + ")"}</span>
+              <p
+                className={`${
+                  index == 0 || index == 8 ? "sm:text-sm lg:text-md" : ""
+                }`}
+              >
                 Patkamon Awaiwanont{" "}
               </p>
-              <a
-                className={`${index == 0 || index == 8 ? " text-sm" : ""}`}
-                href="#"
-              >
-                link
-              </a>
             </div>
           );
         })}
       </div>
       <div className="flex flex-col items-center">
-        <h1 className="pt-6 pb-0 md:text-[30px] lg:text-[44px] ">
+        <h1 className="pt-6 pb-0 md:text-[22px] lg:text-[36px] ">
           {" "}
           &#60;Election&#62; Candidate
         </h1>
-        <div class=" flex flex-col items-center mt-4 mb-auto max-w-sm bg-[#2A5DAA] rounded-md shadow-md dark:border-gray-700  w-[37.75rem] h-[41.5rem]">
+        <div class=" flex flex-col items-center mt-4 mb-auto max-w-sm bg-[#2A5DAA] rounded-md shadow-md dark:border-gray-700  w-[37.75rem]  h-[41.5rem]  md:w-[32.75rem]  md:h-[36.5rem]  ">
           <img
             class="rounded my-6"
-            // src="https://www.eng.ku.ac.th/wp-content/uploads/2020/11/32-James-Edward-Brucker.jpg"
+            src="https://www.eng.ku.ac.th/wp-content/uploads/2020/11/32-James-Edward-Brucker.jpg"
             width={300}
             height={400}
           />
@@ -91,7 +89,7 @@ function Candidate() {
             <p class="mb-2 text-2xl font-bold tracking-tight text-[#fff] dark:text-white w-[300px] flex justify-center">
               James Edward Brucker
             </p>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 w-[22rem] h-[12.5rem] bg-[#fff] rounded-md my-2">
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 w-[22rem] h-[12.5rem] md:text-sm md:w-9/10 md:h-32 bg-[#fff] rounded-md my-2">
               ตำแหน่งทางวิชาการ: Software Engineering Specialist การศึกษา: Ph.D
               ( Electrical Engineering ), University of California , 1986 M.A (
               Mathematics ), University of Hawaii , 1981 M.A ( Statistics ),
