ed381e630fc76adf9f7e13f32602b806667d07c5
patkamon <patkamon.a@ku.th>
2022-10-20 11:10:10 +0700

    done

--- pages/candidate/index.js
+++ pages/candidate/index.js
@@ -76,8 +76,25 @@ function Candidate() {
         })}
       </div>
 
-      <div className="bg-cyan-300 w-96 h-96 my-auto">
-        {/* TODO: TOUCH ADD HERE */}
+      <div class="flex flex-col items-center my-auto max-w-sm bg-[#2A5DAA] rounded-md shadow-md dark:border-gray-700  w-[37.75rem] h-[41.5rem]">
+        <img
+          class="rounded my-6"
+          src="https://www.eng.ku.ac.th/wp-content/uploads/2020/11/32-James-Edward-Brucker.jpg"
+          width={300}
+          height={400}
+        />
+        <div class="p-5 flex flex-col items-center -my-8">
+          <p class="mb-2 text-2xl font-bold tracking-tight text-[#fff] dark:text-white w-[300px] flex justify-center">
+            James Edward Brucker
+          </p>
+          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 w-[22rem] h-[12.5rem] bg-[#fff] rounded-md my-2">
+            ตำแหน่งทางวิชาการ: Software Engineering Specialist การศึกษา: Ph.D (
+            Electrical Engineering ), University of California , 1986 M.A (
+            Mathematics ), University of Hawaii , 1981 M.A ( Statistics ),
+            University of California , 1978 B.A. ( Mathematics), Johns Hopkins
+            University, 1977
+          </p>
+        </div>
       </div>
     </div>
   );
