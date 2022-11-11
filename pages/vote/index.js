cb1549d8bdbe24dd89e156f813816eac1a9e0510
HuskyIsHere <lakagul.peem@gmail.com>
2022-11-11 21:22:44 +0700

    Remove unused dependencies.

--- pages/vote/index.js
+++ pages/vote/index.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import classNames from 'classnames'
 
 const Vote = () => {
   const [inputs, setInputs] = useState({});
