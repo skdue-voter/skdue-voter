e5b16cabd3694188d7ad926a21d237e25a7e32ef
Thanabardi <thanabardi@hotmail.com>
2022-11-12 23:47:44 +0700

    improved home page

--- pages/home/index.js
+++ pages/home/index.js
@@ -3,70 +3,53 @@ import React, { useState, useEffect } from "react";
 function Home() {
 
   return (
-    <div className="flex flex-col items-center">
+    <div>
       <div className="bg-green w-full absolute ">
-        <a className="absolute p-6 text-yellow" href="../">
-          Log Out
+        <a className="absolute right-0 m-6 text-yellow text-xl" href="../home">
+          Logout
         </a>
-        <h1 className="text-5xl font-medium text-white text-center pt-6">Home Page</h1>
+        <h1 className="text-5xl font-medium text-white text-center mt-4">Name Surname</h1>
       </div>
-      <svg width="100%">
-        <ellipse cx="50%" cy="32%" rx="52%" ry="60%" className="fill-green" />
+      <svg width="100%" height="140">
+        <ellipse cx="50%" cy="32%" rx="52%" ry="55%" className="fill-green" />
       </svg>
 
-      <div className="grid grid-cols-2 gap-4">
-        <div className="1/3">
-          <div className="bg-white rounded-15 float-right w-2/3 p-1.5 border border-gray-dark">
-            <p>Make Thailand great again! Make Thailand great again! Make Thailand great again! Make Thailand great again!
-              Make Thailand great again! Make Thailand great again! Make Thailand great again! Make Thailand great again!
-              Make Thailand great again! Make Thailand great again! Make Thailand great again! Make Thailand great again!
-              Make Thailand great again! Make Thailand great again! Make Thailand great again! Make Thailand great again!
-              Make Thailand great again! Make Thailand great again! Make Thailand great again! Make Thailand great again!
-              Make Thailand great again! Make Thailand great again! Make Thailand great again! Make Thailand great again!
-              Make Thailand great again! Make Thailand great again! Make Thailand great again! Make Thailand great again!
-              Make Thailand great again! Make Thailand great again! Make Thailand great again! Make Thailand great again!
-              Make Thailand great again! Make Thailand great again! Make Thailand great again! Make Thailand great again!
-              Make Thailand great again! Make Thailand great again! Make Thailand great again! Make Thailand great again!
-              Make Thailand great again! Make Thailand great again! Make Thailand great again! Make Thailand great again!
-              Make Thailand great again! Make Thailand great again! Make Thailand great again! Make Thailand great again!
-              Make Thailand great again! Make Thailand great again! Make Thailand great again! Make Thailand great again!
-              Make Thailand great again! Make Thailand great again! Make Thailand great again! Make Thailand great again!
-              Make Thailand great again! Make Thailand great again! Make Thailand great again! Make Thailand great again!
-            </p>
-          </div>
+      <div className="grid grid-cols-5 gap-4 my-5 w-[700px] xl:w-[900px] m-auto">
+        <div className="bg-white rounded-lg p-1.5 border border-gray-dark col-span-2">
+          <p>Voter Info</p>
         </div>
 
-        <div className="bg-white relative rounded-15 w-2/3 p-1.5 border border-gray-dark h-2/3 h-[43rem]">
-          <h1 className="text-3xl">Donald Trump</h1>
-          <img className="max-h-96 rounded" src="trump.jpg" class="img-fluid" alt="..."></img>
-          <b className="mt-3 mb-3 left-3">Make Thailand great again!</b>
+        <div className="bg-white rounded-lg border border-gray-dark col-span-3">
+          <h1 className="text-3xl font-semibold p-2 items-center">Donald Trump's Election</h1>
+          <img className="w-full" src="trump.jpg" alt="..."></img>
+          <p className="m-3">Make Thailand great again!</p>
+          <hr className="my-3 h-px bg-gray border-0" />
 
-          <div class="mx-4 bg-green-200 p-2">
-            <div class="flex flex-col-reverse">
-              <div className="text-red mt-4 text-xl font-medium">Vote Pending
-                <button type="submit"
-                  className="bg-gray w-32 mt-0 absolute right-3 rounded-10 p-1 my-1 w-full text-xl font-medium hover:opacity-80"
-                >Party List</button></div>
-                <div class="flex flex-col-reverse">
-              <div className="text-xl mt-6 font-medium">Vote Party
-                <button type="submit"
-                  className="bg-yellow-lemon w-32 mt-0 absolute right-3 rounded-10 p-1 my-1 w-full text-xl font-medium hover:opacity-80"
-                >Vote</button></div>
-              <div className="text-green mt-4 text-xl font-medium">Voted for James Broker
-                <button type="submit"
-                  className="bg-gray w-32 mt-0 absolute right-3 rounded-10 p-1 my-1 w-full text-xl font-medium hover:opacity-80"
-                >Candidate</button></div>
-                </div>
-              <div className="text-xl mt-4 font-medium">Vote Candidate
-                <button type="submit"
-                  className="bg-green w-32 mt-0 absolute right-3 rounded-10 p-1 my-1 w-full text-xl font-medium hover:opacity-80"
-                >Change</button></div>
-                </div>
+          <div className="grid grid-cols-4 grid-rows-2 gap-2 px-3 text-lg font-medium">
+            <p className="col-span-3">Vote Candidate</p>
+            <button type="submit" 
+              className="bg-green-lime py-0.5 rounded-md hover:brightness-90">Change</button>
+            <p className="text-green-lime col-span-3 font-normal">Voted for Kim Un Jong</p>
+            <button type="submit"
+              className="bg-gray py-0.5 text-lg font-medium rounded-md hover:brightness-90">Candidate</button>
           </div>
-          <p className="text-red mt-4 mb-2 text-center">Only Voted for Candidate</p>
+          <hr className="my-3 h-px bg-gray border-0" />
+
+          <div className="grid grid-cols-4 grid-rows-2 gap-2 px-3 text-lg font-medium">
+            <p className="col-span-3">Vote Candidate</p>
+            <button type="submit" 
+              className="bg-yellow-lemon py-0.5 rounded-md hover:brightness-90">Vote</button>
+            <p className="text-red col-span-3 font-normal">Vote pending</p>
+            <button type="submit"
+              className="bg-gray py-0.5 text-lg font-medium rounded-md hover:brightness-90">Party List</button>
+          </div>
+          <hr className="my-3 h-px bg-gray border-0" />
+
+          <p className="text-red mb-1 text-center">Only Voted for Candidate</p>
+          <div className="m-1">
           <button type="submit"
-            className="text-white bg-red w-11/12 p-2 ml-5 rounded-10 text-xl font-medium hover:opacity-80"
-          >Confirm Vote</button>
+            className="text-white bg-red rounded-md p-3 w-full text-xl font-medium hover:brightness-90"
+          >Confirm Vote</button></div>
         </div>
       </div>
     </div>
