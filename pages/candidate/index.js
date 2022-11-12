96f927644e1db56cd552fde428f76d12abd43446
Thanabardi <thanabardi@hotmail.com>
2022-11-13 01:44:22 +0700

    improved candidate and party list page ui

--- pages/candidate/index.js
+++ pages/candidate/index.js
@@ -19,6 +19,12 @@ function Candidate() {
     4,
   ]);
 
+  const handleCandidateChange = (event) => {
+    const index = event.target.value
+    setMainIndex(index);
+    changeCards(index);
+  };
+
   useEffect(() => {
     setLen(cards.length);
     console.log("set length", len);
@@ -42,22 +48,12 @@ function Candidate() {
 
   return (
     <div className="body flex h-screen ">
-      <a className="absolute p-4 text-yellow" href="../">
+      <a className="absolute m-4 text-yellow text-xl font-semibold" href="../home">
         Back
       </a>
       <div className="absolute top-0 w-[22rem] sm:1/5 mt-0 h-full -z-10 bg-green">
-        <svg
-          className="ml-36 xl:ml-50 lg:ml-60 md:ml-48 sm:ml-40  "
-          width="100%"
-          height="100%"
-        >
-          <ellipse
-            cx="-60%"
-            cy="50%"
-            rx="122%"
-            ry="80%"
-            className="fill-green"
-          />
+        <svg width="100%" height="100%"className="ml-36 xl:ml-50 lg:ml-60 md:ml-48 sm:ml-40">
+          <ellipse cx="-60%" cy="50%" rx="122%" ry="80%" className="fill-green" />
         </svg>
       </div>
       <div
@@ -71,33 +67,41 @@ function Candidate() {
         //     changeCards(Math.min(len, _index++));
         //   }
         // }}
-        className=" outer flex flex-col justify-center w-1/2 h-[90%] ml-14 my-auto gap-4  overflow-y-scroll"
+        className="outer flex flex-col justify-center w-2/5 h-[90%] ml-6 my-auto gap-4 p-2"
       >
+        <input
+          className="fixed w-[37%] 2xl:w-[40%] h-3 bg-white/80 rounded-sm appearance-none cursor-pointer range-lg top-20 origin-left transform rotate-90"
+          type="range"
+          min="0"
+          max={cards.length-1}
+          value={mainIndex}
+          onChange={handleCandidateChange}
+        />
         {displayCards.map((card, index) => {
           let main = mainIndex;
           return (
-            <div key={index} className="flex flex-row gap-5 ">
+            <div key={index} className="flex flex-row gap-5 ml-8">
               <div
-                className={`card flex flex-row border rounded bg-white max-h-44 
+                className={`card flex flex-row rounded-md bg-white max-h-44 
               ${card == null ? "invisible" : ""}
                  ${
                    index == 4
-                     ? "font-semibold w-1/2 max-w-[22rem] md:py-5"
+                     ? "font-semibold w-1/2 md:py-5 max-w-[22rem] "
                      : "opacity-60 "
                  }
                ${
                  (index == 1 || index == 7) && card != main
-                   ? "  w-2/5 md:py-3 max-w-[18rem]"
+                   ? "w-2/5 md:py-3 max-w-[18rem]"
                    : ""
                }
               ${
                 (index == 0 || index == 8) && card != main
-                  ? " w-1/3 md:py-2 max-w-[16rem]"
+                  ? "w-1/3 md:py-2 max-w-[16rem]"
                   : "md:py-5"
               }
               ${
                 index == 2 || index == 3 || index == 5 || index == 6
-                  ? "w-[45%]  md:py-4 max-w-[20rem]"
+                  ? "w-[45%] md:py-4 max-w-[20rem]"
                   : ""
               }
               
@@ -122,38 +126,31 @@ function Candidate() {
                   card != main ? "opacity-0" : ""
                 } text-white font-medium text-3xl`}
               >
-                {" "}
                 {">"}
               </div>
             </div>
           );
         })}
       </div>
-      <div className="flex flex-col items-center justify-center overflow-auto ">
-        <h1 className="pt-6 pb-0 md:text-[22px] lg:text-[36px] ">
-          {" "}
-          &#60;Election&#62; Candidate
-        </h1>
-        <div class="flex flex-col items-center bg-party-blue  rounded-15  mb-2 pb-2 ">
-          <img
-            class="rounded-15 p-2 lg:w-[360px] "
+      <div className="flex flex-col items-center justify-center overflow-auto">
+        <h1 className="pb-10 text-5xl font-semibold">Candidate</h1>
+        <div class="bg-party-blue rounded-lg pb-2 text-white w-1/3 p-2">
+          <img class="rounded-md"
             src="https://www.eng.ku.ac.th/wp-content/uploads/2020/11/32-James-Edward-Brucker.jpg"
           />
-          <div class="flex flex-col items-center ">
-            <p class="text-2xl font-bold text-white dark:text-white flex justify-center">
-              James Edward Brucker
-            </p>
-            <p class="text-1xl text-white dark:text-white flex justify-center ">
-              SKEKILLER
-            </p>
-            <p class="p-2 font-normal md:text-sm bg-white rounded-10 w-[21.5rem]">
-              ตำแหน่งทางวิชาการ: Software Engineering Specialist การศึกษา: Ph.D
-              ( Electrical Engineering ), University of California , 1986 M.A (
-              Mathematics ), University of Hawaii , 1981 M.A ( Statistics ),
-              University of California , 1978 B.A. ( Mathematics), Johns Hopkins
-              University, 1977
-            </p>
-          </div>
+          <p class="text-2xl font-bold dark:text-white text-center pt-2">
+            James Edward Brucker
+          </p>
+          <p class="text-1xl dark:text-white text-center pb-2">
+            SKEKILLER
+          </p>
+          <p class="p-2 text-sm bg-white text-black rounded-md">
+            ตำแหน่งทางวิชาการ: Software Engineering Specialist การศึกษา: Ph.D
+            ( Electrical Engineering ), University of California , 1986 M.A (
+            Mathematics ), University of Hawaii , 1981 M.A ( Statistics ),
+            University of California , 1978 B.A. ( Mathematics), Johns Hopkins
+            University, 1977
+          </p>
         </div>
       </div>
     </div>
