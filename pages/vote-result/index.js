b28bc02c27852f30242fc040e4ebd62dc7cbae28
Thanabardi <thanabardi@hotmail.com>
2022-11-22 16:33:52 +0700

    add party result

--- pages/vote-result/index.js
+++ pages/vote-result/index.js
@@ -4,33 +4,45 @@ import { useRouter } from 'next/router'
 
 const VoteResult = () => {
   const router = useRouter()
-  const [area, setArea] = useState([1, 2, 3, 4]);
+  const [area, setArea] = useState([]);
   const [selectArea, setSelectArea] = useState();
   const [selectType, setSelectType] = useState(0);
-  const [resultData, setResultData] = useState([]);
+  const [candidateList, setCandidateList] = useState([]);
+  const [partyList, setPartyList] = useState([]);
 
   useEffect(() => {
-    let user = JSON.parse(sessionStorage.getItem("user"));
-    getCandidateList();
+    getArea()
+    getCandidateList()
+    getPartyList()
   }, []);
+
+  async function getArea() {
+    await axios.get(`https://sankasaint.helloyeew.dev/api/area`)
+      .then((response) => {
+        setArea(response.data.result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
   async function getCandidateList(area) {
     await axios.get(`https://sankasaint.helloyeew.dev/api/election/2/result/area/${area?area:1}`)
       .then((response) => {
-        setResultData(response.data.vote_result);
+        setCandidateList(response.data.vote_result);
       })
       .catch((error) => {
-        window.alert(error);
+        console.log(error);
       });
   }
 
-  async function getPartyList(area) {
-    await axios.get(`https://sankasaint.helloyeew.dev/api/election/2/result/area/${area?area:1}`)
+  async function getPartyList() {
+    await axios.get(`https://sankasaint.helloyeew.dev/api/election/2/result/party`)
       .then((response) => {
-        setResultData(response.data.vote_result);
+        setPartyList(response.data.vote_result);
       })
       .catch((error) => {
-        window.alert(error);
+        console.log(error);
       });
   }
 
@@ -43,11 +55,6 @@ const VoteResult = () => {
   function handleSelectType(value) {
     if (value !== selectType) {
       setSelectType(value)
-      if (value === 0) {
-        getCandidateList(selectArea)
-      } else {
-        getPartyList(selectArea)
-      }
     }
   }
 
@@ -63,15 +70,7 @@ const VoteResult = () => {
         <ellipse cx="50%" cy="32%" rx="52%" ry="55%" className="fill-green" />
       </svg>
 
-      <div className="flex bg-white rounded-lg border border-gray-dark p-1 w-[50rem] m-auto mt-2">
-        <p className="my-2 mx-4 text-xl font-semibold">Area</p>
-        <select value={selectArea} onChange={handleSelectArea} class="bg-gray text-sm rounded-md w-20 p-2">
-          {area.map((area, index) => { return (<option>{area}</option>) })}
-        </select>
-        <p className="my-2 mx-4 text-xl">{resultData[0] && resultData[0].candidate.area.name}</p>
-      </div>
-
-      <div className="flex bg-gray rounded-full w-[50rem] m-auto mt-4 text-xl font-semibold">
+      <div className="flex bg-gray rounded-full w-[50rem] m-auto mt-2 text-xl font-semibold">
         <button
           type="submit"
           onClick={(e) => {handleSelectType(0)}}
@@ -83,27 +82,50 @@ const VoteResult = () => {
           className={`p-2 rounded-full w-1/2 ${selectType === 1 ? "bg-white drop-shadow-md" : "opacity-60"}`}
         >Party</button>
       </div>
+
+      {selectType == 0 && <div className="flex bg-white rounded-lg border border-gray-dark p-1 w-[50rem] m-auto mt-4">
+        <p className="my-2 mx-4 text-xl font-semibold">Area</p>
+        <select value={selectArea} onChange={handleSelectArea} className="bg-gray text-sm rounded-md w-20 p-2">
+          {area.map((area, index) => { return (<option>{area.id}</option>) })}
+        </select>
+        <p className="my-2 mx-4 text-xl">{candidateList[0] && candidateList[0].candidate.area.name}</p>
+      </div>}
 			
       <div className="grid grid-cols-1 gap-4 m-auto my-4 w-[50rem] h-[36rem] overflow-auto">
-        {resultData.map((data, index) => {
+        {selectType === 0 ? candidateList.map((data, index) => {
           return (
             <div className="flex bg-white rounded-lg border border-gray-dark">
-              <p className="flex-none rounded-l-lg text-center px-2 pt-2 font-semibold text-xl text-white bg-green">{data.vote_count}</p>
+              <p className="flex-none rounded-l-lg text-center px-2 pt-2 font-semibold text-xl text-white bg-green">
+                {data.vote_count}</p>
               <img
                 className="flex-none object-cover w-44 h-full"
                 src={data.candidate.image}
               />
               <div className="flex-auto my-2 mx-4">
                 <p className="text-2xl font-semibold">{data.candidate.user.first_name} {data.candidate.user.last_name}</p>
-                <p className="text-lg mb-4">{data.candidate.description}</p>
-                <div className="text-md opacity-80">
-                  <p>Party: {data.candidate.party?c.candidate.party:"Liberal"}</p>
-                  <p>Area: {data.candidate.area.name}</p>
+                <div className="text-md opacity-80 mt-4">
+                  <p>Party: {data.candidate.party?data.candidate.party.name:"Liberal"}</p>
+                  <p>Area: {data?.candidate.area.name}</p>
                 </div>
               </div>
             </div>
-          );
-        })}
+          )}) : partyList.map((data, index) => {
+            return (
+              <div className="flex bg-white rounded-lg border border-gray-dark">
+                <p className="flex-none rounded-l-lg text-center px-2 pt-2 font-semibold text-xl text-white bg-green">
+                  {data.real_result}</p>
+                <img
+                  className="flex-none object-cover w-44 h-full"
+                  src={data.party.image}
+                />
+                <div className="flex-auto my-2 mx-4">
+                  <p className="text-2xl font-semibold">{data.party.name}</p>
+                  <p className="text-lg mb-4">{data.party.quote}</p>
+                </div>
+              </div>
+            )
+          })
+        }
 			</div>
     </div>
   );
